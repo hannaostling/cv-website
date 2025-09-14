fbf0e63584a8a6ebec62e08b340f88df72991482
hannaostling <hannaoliviaostling@gmail.com>
2025-09-14 16:05:07 +0200

    Add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import "./globals.css";
 import type { ReactNode } from "react";
 
+export const metadata = {
+  title: "Hanna Ösling - CV",
+  icons: {
+    icon: "/icons/FavIcon.png",
+  },
+};
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="sv" suppressHydrationWarning>
